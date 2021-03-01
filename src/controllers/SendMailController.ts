bdf08e3cb2e924c0427d8a22efdb9d649223beed
jairpro <jairedias@yahoo.com.br>
2021-03-01 03:47:29 -0300

    Update SendMailController.ts
    corrige condição de or para and

--- src/controllers/SendMailController.ts
+++ src/controllers/SendMailController.ts
@@ -34,7 +34,7 @@ class SendMailController {
     }
 
     const surveyUserAlreadyExists = await surveysUsersRepository.findOne({
-      where: [{ user_id: user.id }, { value: null }],
+      where: { user_id: user.id , value: null },
       relations: ["user", "survey"],
     })
 
